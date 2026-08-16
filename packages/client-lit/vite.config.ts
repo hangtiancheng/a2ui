5651767aaf68ec8b504e66d0de46661bff98dd5a
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-16 22:23:33 +0800

    Introduce new features

---
+++ packages/client-lit/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+import * as Middleware from "./middleware/index.js";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [Middleware.A2AMiddleware.plugin()],
+  server: {
+    port: 5003,
+    strictPort: true,
+  },
+});
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import { plugin as a2aPlugin } from "./middleware/a2a.js";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), a2aPlugin()],
+    server: {
+    port: 5003,
+    strictPort: true,
+  },
 });
