e10cf864240a884ffb4360c4ca7d3cc5e534eecf
hangtiancheng.htc <hangtiancheng.htc@taobao.com>
2026-08-16 22:56:19 +0800

    Introduce new features

--- packages/client-lit/vite.config.ts
+++ packages/client-lit/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 
 import * as Middleware from "./middleware/index.js";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Middleware.A2AMiddleware.plugin()],
+  plugins: [
+    Middleware.A2AMiddleware.plugin(),
+
+    tailwindcss(),
+  ],
   server: {
     port: 5004,
     strictPort: true,
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { plugin as a2aPlugin } from "./middleware/a2a.js";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), a2aPlugin()],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), a2aPlugin(), tailwindcss()],
   server: {
     port: 5003,
     strictPort: true,
